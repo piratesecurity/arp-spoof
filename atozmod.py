a3a1d9171ebf06ddc893e4f4afbf13c1325879a9
Bharath Ambati <bharath.hussy@gmail.com>
2015-04-13 06:15:14 +0530

    atozmod.py
    Customized Module for getting IP and MAC address of Devices

---
+++ atozmod.py
@@ -0,0 +1,29 @@
+######################################################################################
+# Custom Developed Module for Arp Spoofing Prevention Method
+# Developed By piratesecurity 
+######################################################################################
+
+import commands
+import netifaces
+#Global Variables
+interface="eth0"
+
+def get_ip_address():	
+	try:
+		words = commands.getoutput("sudo ifconfig " + interface).split()	
+		if "HWaddr" in words:
+			return words[ words.index("HWaddr") + 3 ].split(":")[1]
+	except:
+		print "Unable Get MAC Address of the Device."
+		exit(1)
+		
+
+def get_mac_address():
+	try:
+		words = commands.getoutput("sudo ifconfig " + interface).split()
+		if "HWaddr" in words:
+			return words[ words.index("HWaddr") + 1 ]
+	except:
+		print "Unable Get MAC Address of the Device."
+		exit(1)
+		
